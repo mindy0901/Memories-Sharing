a2d6b2038ca53b100ff581e3798abd72e8260808
mindylonely <milulou2057@gmail.com>
2022-05-01 21:20:16 +0700

    package.json edited

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
